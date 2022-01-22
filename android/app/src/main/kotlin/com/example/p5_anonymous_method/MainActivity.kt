0cc673847c29a4909f1833075106b224906b4d22
Anashaneef <anas.f.hanif@students.amikom.ac.id>
2022-01-22 19:08:49 +0700

    Upload Pertama

---
+++ android/app/src/main/kotlin/com/example/p5_anonymous_method/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p5_anonymous_method
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "Ini adalah teks";
+
+  // void tombolDitekan() {
+  //   setState(() {
+  //     message = "Tombol sudah ditekan";
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              RaisedButton(
+                  child: Text("Tekan Saya"),
+                  onPressed: () {
+                    setState(() {
+                      message = "Tombol sudah ditekan";
+                    });
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
